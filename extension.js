f43c4713d9f749d11deff80628786b2cc2351aa1
Devin Abbott <devinabbott@gmail.com>
2017-06-05 17:39:37 -0700

    Fix diff algorithm and clean up extension code

--- extension.js
+++ extension.js
@@ -78,10 +78,66 @@ function handleMessage(data) {
     vscode.window.showInformationMessage(messages.join('\n'));
   }
 
-  // if ('unresolvedImports' in json) {
-  //   // TODO prompt user
-  //   return;
-  // }
+  if ('unresolvedImports' in json && Object.keys(unresolvedImports).length > 0) {
+    // unresolvedImports.test = [
+    //   {displayName: 'test1', filePath: 't1'},
+    //   {displayName: 'test2', filePath: 't2'},
+    // ]
+
+    const questions = Object.keys(unresolvedImports).map(name => {
+      const matches = unresolvedImports[name];
+      const options = matches.map(({displayName, importPath, filePath}) => {
+        return {label: displayName, description: filePath, importPath};
+      })
+
+      return {name, options}
+    })
+
+    const getAnswers = (remaining, results = []) => {
+      if (remaining.length === 0) {
+        return Promise.resolve(results);
+      }
+
+      const {name, options} = remaining[0];
+
+      return vscode.window.showQuickPick(options, {placeHolder: `Import ${name} from`}).then(selected => {
+        // If user cancels, still import the modules they've resolved so far
+        if (!selected) {
+          return Promise.resolve(results)
+        }
+
+        const answer = {name, path: selected.importPath};
+
+        return getAnswers(remaining.slice(1), results.concat(answer));
+      })
+    }
+
+    getAnswers(questions).then(answers => {
+      console.log('Answers!', answers);
+
+      const imports = answers.reduce((imports, answer) => {
+        const {name, path} = answer
+
+        imports[name] = path
+
+        return imports
+      }, {})
+
+      run('add', imports)
+    })
+
+    // vscode.window.showQuickPick([
+    //   {
+    //     label: 'label',
+    //     description: 'description',
+    //     detail: 'detail',
+    //   }
+    // ], {
+    //   placeHolder: 'Resolve item',
+    // })
+
+    return;
+  }
 
   if ('goto' in json) {
     console.log('opening', goto);
@@ -96,92 +152,47 @@ function handleMessage(data) {
   }
 }
 
-function run(edit, args = {}) {
-  const {cmd} = args
+function run(command, commandArg) {
   const payload = {
-    "command": cmd,
-    "pathToFile": currentFilePath(),
-    "fileContent": currentFileContents(),
+    command: command,
+    commandArg: commandArg,
+    pathToFile: currentFilePath(),
+    fileContent: currentFileContents(),
   }
-
-  if (cmd === 'word' || cmd === 'goto') {
-    payload["commandArg"] = currentWord()
-  }
-
-  console.log('payload', payload)
 
   daemon.stdin.write(JSON.stringify(payload) + '\n')
+}
 
+function startDaemon() {
+  daemon = spawn('importjs', ['start', `--parent-pid=${process.pid}`], {
+    cwd: projectDirectoryPath(),
+  });
 
-  // // process = self.start_or_get_daemon()
-  // // process.stdin.write((json.dumps(payload) + '\n').encode('utf-8'))
-  // // process.stdin.flush()
-  // resultJson = process.stdout.readline().decode('utf-8')
-  // print(resultJson)
+  daemon.stdout.on('data', handleMessage)
 
-  // result = json.loads(resultJson)
+  // daemon.stderr.on('data', (data) => {
+  //   console.log(`stderr: ${data}`);
+  // });
 
-  // if(result.get('error')):
-  //     sublime.error_message(
-  //         'Error when executing importjs:\n\n' + result.get('error'))
-  //     return
+  // daemon.on('close', (code) => {
+  //   console.log(`child process exited with code ${code}`);
+  // });
 
-  // if(result.get('messages')):
-  //     sublime.status_message('\n'.join(result.get('messages')))
-  // if(result.get('unresolvedImports')):
-  //     def handle_resolved_imports(resolved_imports):
-  //         args['command'] = 'add'
-  //         args['imports'] = resolved_imports
-  //         self.run(edit, **args)
-  //     self.view.run_command("import_js_replace",
-  //                           {"characters": result.get('fileContent')})
-  //     self.ask_to_resolve(result.get('unresolvedImports'),
-  //                         handle_resolved_imports)
-  //     return
-
-  // if(cmd == 'goto'):
-  //     self.view.window().open_file(result.get('goto'))
-  // else:
-  //     self.view.run_command("import_js_replace",
-  //                           {"characters": result.get('fileContent')})
+  // console.log('File path:', currentFilePath(), projectDirectoryPath())
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    context.subscriptions.push(
-      vscode.commands.registerCommand('importjs.word', () => {
-        run(null, {cmd: 'word'})
-      })
-    )
+  const subscriptions = [
+    vscode.commands.registerCommand('importjs.word', () => run('word', currentWord())),
+    vscode.commands.registerCommand('importjs.goto', () => run('goto', currentWord())),
+    vscode.commands.registerCommand('importjs.fix', () => run('fix')),
+  ]
 
-    context.subscriptions.push(
-      vscode.commands.registerCommand('importjs.goto', () => {
-        run(null, {cmd: 'goto'})
-      })
-    )
+  subscriptions.forEach(sub => context.subscriptions.push(sub))
 
-    context.subscriptions.push(
-      vscode.commands.registerCommand('importjs.fix', () => {
-        run(null, {cmd: 'fix'})
-      })
-    )
-
-    daemon = spawn('importjs', ['start', `--parent-pid=${process.pid}`], {
-      cwd: projectDirectoryPath(),
-    });
-
-    daemon.stdout.on('data', handleMessage)
-
-    daemon.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`);
-    });
-
-    daemon.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
-
-    console.log('File path:', currentFilePath(), projectDirectoryPath())
+  startDaemon()
 }
 
 exports.activate = activate;
