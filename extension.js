92f0c791e719541689874de1e919c533d42cbc33
Devin Abbott <devinabbott@gmail.com>
2017-06-06 14:02:00 -0700

    Refactor into multiple files

--- extension.js
+++ extension.js
@@ -1,79 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 
-const { WorkspaceEdit } = vscode;
+const { syncFileContents, currentWord } = require("./lib/project");
+const { start, kill, run, on } = require("./lib/daemon");
 
-const { spawn, exec } = require("child_process");
-const oboe = require("oboe");
-
-const textEditsForDiff = require("./lib/textEditsForDiff");
-
-let daemon = null;
-
-function activeEditor() {
-  return vscode.window.activeTextEditor;
-}
-
-function currentDocument() {
-  return activeEditor().document;
-}
-
-function currentFilePath() {
-  return currentDocument().fileName;
-}
-
-function currentFileContents() {
-  return currentDocument().getText();
-}
-
-function applyEdit(edit) {
-  const workspaceEdit = new WorkspaceEdit();
-  workspaceEdit.set(currentDocument().uri, [edit]);
-  vscode.workspace.applyEdit(workspaceEdit);
-}
-
-function syncFileContents(newText) {
-  const currentText = currentFileContents();
-
-  if (currentText === newText) return;
-
-  const textEdits = textEditsForDiff(currentText, newText);
-  textEdits.forEach(applyEdit);
-}
-
-// https://github.com/codemirror/CodeMirror/blob/master/mode/javascript/javascript.js#L25
-const wordRE = /[\w$\xa1-\uffff]+/;
-
-function currentWord() {
-  const position = activeEditor().selection.active;
-  const range = currentDocument().getWordRangeAtPosition(position, wordRE);
-
-  if (!range) {
-    return null;
-  }
-
-  return currentDocument().getText(range);
-}
-
-function projectDirectoryPath() {
-  return vscode.workspace.rootPath;
-}
+const STATUS_BAR_DELAY = 3000;
 
 function handleMessage(json) {
   const { messages, fileContent, unresolvedImports, error, goto } = json;
 
   if ("error" in json) {
-    console.log("Error executing importjs", error);
+    vscode.window.showWarningMessage(
+      `ImportJS encountered an error: ${error.message}`
+    );
     return;
   }
 
-  if ("messages" in json && messages.length) {
-    vscode.window.setStatusBarMessage(messages.join("\n"), 3000);
+  if ("messages" in json && messages.length > 0) {
+    vscode.window.setStatusBarMessage(messages.join("\n"), STATUS_BAR_DELAY);
   }
 
   if ("goto" in json) {
-    console.log("opening", goto);
     vscode.workspace.openTextDocument(goto).then(document => {
       vscode.window.showTextDocument(document);
     });
@@ -134,78 +80,6 @@ function handleMessage(json) {
   }
 }
 
-function run(command, commandArg) {
-  if (!daemon) {
-    startDaemon();
-  }
-
-  const payload = {
-    command: command,
-    commandArg: commandArg,
-    pathToFile: currentFilePath(),
-    fileContent: currentFileContents()
-  };
-
-  daemon.stdin.write(JSON.stringify(payload) + "\n");
-
-  console.log('run', command, 'ok')
-}
-
-function getImportJSPath() {
-  return new Promise((resolve, reject) => {
-    try {
-      exec("which importjs", (error, stdout) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(stdout);
-      }
-    });
-    } catch (error) {
-      reject(error)
-    }
-  });
-}
-
-function startDaemon() {
-  const failedToStart = error => {
-    daemon = null;
-    console.warn("Failed to start ImportJS server", error);
-
-    getImportJSPath()
-      .then(() => {
-        vscode.window.showWarningMessage(
-          `Failed to start ImportJS server: ${error.message}`
-        );
-      })
-      .catch(() => {
-        vscode.window.showWarningMessage(
-          `Failed to start ImportJS server: importjs not found in PATH`
-        );
-      });
-  };
-
-  try {
-    daemon = spawn("importjs", ["start", `--parent-pid=${process.pid}`], {
-      cwd: projectDirectoryPath()
-    });
-
-    daemon.on("error", failedToStart);
-    daemon.on("close", () => {
-      daemon = null
-    })
-  } catch (error) {
-    failedToStart(error);
-    return;
-  }
-
-  // Ignore the first message passed. This just gives the log path, and isn't json.
-  daemon.stdout.once("data", () => {
-    // After the first message, handle top-level JSON objects
-    oboe(daemon.stdout).node("!", handleMessage);
-  });
-}
-
 function activate(context) {
   const subscriptions = [
     vscode.commands.registerCommand("importjs.word", () =>
@@ -219,13 +93,13 @@ function activate(context) {
 
   subscriptions.forEach(sub => context.subscriptions.push(sub));
 
-  startDaemon();
+  on("message", handleMessage);
+
+  start();
 }
 
 function deactivate() {
-  if (daemon) {
-    daemon.kill();
-  }
+  kill();
 }
 
 exports.activate = activate;
